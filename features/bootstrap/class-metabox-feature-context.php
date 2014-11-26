4a48b8ad8ddb85fb9942548813af06c0caa5e19f
tmf <tom.forrer@gmail.com>
2014-11-26 14:51:58 +0100

    fixture plugin for behat tests

---
+++ features/bootstrap/class-metabox-feature-context.php
@@ -0,0 +1,11 @@
+<?php
+
+use Tmf\WordPressExtension\Context\WordPressContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class MetaboxFeatureContext extends WordPressContext
+{
+
+}
---
+++ features/bootstrap/plugins/metabox-test/metabox-test.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: Metabox Helper Test
+ * Plugin URI: http://github.com/tmf/wp-metabox-helper
+ * Description: A Test Plugin for the WordPress Metaboxhelper service
+ * Version: 0.1
+ * Author: Tom Forrer
+ * Author URI: http://githubm.com/tmf
+ * License: MIT
+ */
+
+/**
+ * @autor     Tom Forrer <tom.forrer@gmail.com>
+ * @copyright Copyright (c) 2014 Tom Forrer (http://github.com/tmf)
+ */
+if(!file_exists(__DIR__ . '/base')){
+    symlink(__DIR__ . '/../../../..', __DIR__ . '/base');
+}
+
+if (file_exists( __DIR__ . '/base/vendor/autoload.php')) {
+    require_once __DIR__ . '/base/vendor/autoload.php';
+}
+
+use Tmf\Wordpress\Service\Metabox\MetaboxServiceProvider,
+    Tmf\Wordpress\Service\Metabox\Metabox,
+    Tmf\Wordpress\Service\Metabox\Item\InputItem,
+    Tmf\Wordpress\Service\Metabox\Item\DropdownItem,
+    Tmf\Wordpress\Service\Metabox\Item\EditorItem;
+
+$services = new Pimple\Container();
+
+// $plugin contains the path of this plugin as wordpress sees it
+$services->register(new MetaboxServiceProvider(), ['metaboxes.base_directory' => dirname($plugin) . '/base']);
+
+add_action('admin_init', function () use ($services) {
+    $services['metaboxes']['foo'] = new Metabox('Foo', ['post'], 'normal', 'high');
+    $services['metaboxes']['foo']['text'] = new InputItem(['label' => 'Metatext', 'description' => 'Some description']);
+    $services['metaboxes']['foo']['dropdown'] = new DropdownItem(['multiple' => false, 'options' => [['label' => 'Foo', 'value'=>'foo'], ['label' => 'ASDF', 'value'=>'asdf']]]);
+    $services['metaboxes']['foo']['editor'] = new EditorItem(['label' => 'Editor']);
+});
