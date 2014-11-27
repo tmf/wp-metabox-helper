edc2aa1b6bee9a150aafe007440d4c22e231ee0c
tmf <tom.forrer@gmail.com>
2014-11-27 13:54:15 +0100

    timeout is in us: wait more

--- features/bootstrap/class-metabox-feature-context.php
+++ features/bootstrap/class-metabox-feature-context.php
@@ -23,7 +23,8 @@ class MetaboxFeatureContext extends WordPressContext implements SnippetAccepting
     public function iSelectFromTheDropdown($item, $label)
     {
         $session = $this->getSession();
-        $session->wait(10000, 'typeof jQuery.fn.selectize === "function"');
+        // wait 5 seconds (in us)
+        $session->wait(5000000, 'typeof jQuery.fn.selectize === "function"');
         $session->executeScript(sprintf('jQuery("label:contains(\"%s\") + select")[0].selectize.addItem("%s")', $label, $item));
     }
 }
